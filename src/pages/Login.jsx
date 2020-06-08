067584034946c0b90693e603db657acf831776df
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-08 20:47:47 -0300

    Code Climate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,7 @@ class Login extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.createInputPlayerName = this.createInputPlayerName.bind(this);
     this.createInputEmail = this.createInputEmail.bind(this);
+    this.cardButtons = this.cardButtons.bind(this);
   }
 
   async handleChange(e) {
@@ -63,8 +64,34 @@ class Login extends Component {
     );
   }
 
-  render() {
+  cardButtons() {
     const { enableButton } = this.state;
+    return (
+      <div className="card-action">
+        <div className="row">
+          <button
+            type="button"
+            disabled={enableButton}
+            className="waves-effect grey darken-3 btn col s4 offset-s4"
+            data-testid="btn-play"
+          >
+            Play
+          </button>
+          <Link
+            to="/settings"
+            data-testid="btn-settings valign-wrapper"
+            className="col offset-s2 s1"
+          >
+            <i className="material-icons" style={{ verticalAlign: '-12px' }}>
+              settings
+            </i>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <div className="row">
         <div className="col s6 offset-s3">
@@ -72,27 +99,7 @@ class Login extends Component {
             <div className="card-content white-text">
               {this.createInputPlayerName()}
               {this.createInputEmail()}
-            </div>
-            <div className="card-action">
-              <div className="row">
-                <button
-                  type="button"
-                  disabled={enableButton}
-                  className="waves-effect grey darken-3 btn col s4 offset-s4"
-                  data-testid="btn-play"
-                >
-                  Play
-                </button>
-                <Link
-                  to="/settings"
-                  data-testid="btn-settings valign-wrapper"
-                  className="col offset-s2 s1"
-                >
-                  <i className="material-icons" style={{ verticalAlign: '-12px' }}>
-                    settings
-                  </i>
-                </Link>
-              </div>
+              {this.cardButtons()}
             </div>
           </div>
         </div>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
 
 function Settings() {
+  const difficultSelect = () => (
+    <div>
+      <div className="input-field col s12">
+        <select className="browser-default" id="difficult">
+          <option value="" disabled selected>
+            Choose your option
+          </option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <label htmlFor="difficult">Difficult</label>
+    </div>
+  );
+
+  const typeSelect = () => (
+    <div>
+      <div className="input-field col s12">
+        <select className="browser-default" id="type">
+          <option value="" disabled selected>
+            Choose your option
+          </option>
+          <option value="multiple">Multiple Choice</option>
+          <option value="trueOrFalse">True or False</option>
+        </select>
+      </div>
+      <label htmlFor="type">Type</label>
+    </div>
+  );
+
   return (
     <div className="container">
       <div className="row grey darken-3">
@@ -8,31 +39,8 @@ function Settings() {
           <h3 data-testid="settings-title">Settings</h3>
           <div className="col s6 offset-s3">
             <h4>Category</h4>
-            <div>
-              <div className="input-field col s12">
-                <select className="browser-default" id="difficult">
-                  <option value="" disabled selected>
-                    Choose your option
-                  </option>
-                  <option value="easy">Easy</option>
-                  <option value="medium">Medium</option>
-                  <option value="hard">Hard</option>
-                </select>
-              </div>
-              <label htmlFor="difficult">Difficult</label>
-            </div>
-            <div>
-              <div className="input-field col s12">
-                <select className="browser-default" id="type">
-                  <option value="" disabled selected>
-                    Choose your option
-                  </option>
-                  <option value="multiple">Multiple Choice</option>
-                  <option value="trueOrFalse">True or False</option>
-                </select>
-              </div>
-              <label htmlFor="type">Type</label>
-            </div>
+            {difficultSelect()}
+            {typeSelect()}
           </div>
         </div>
       </div>
