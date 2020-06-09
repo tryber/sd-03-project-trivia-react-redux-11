17a1c2d7128c2677bfb5ab8a803230cbb873a0f7
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-09 11:47:09 -0300

    Trocando variaveis Header para TriviaHeader

--- src/components/TriviaHeader.jsx
+++ src/components/TriviaHeader.jsx
@@ -27,15 +27,15 @@ const mapStateToProps = ({ gameReducer: { score, name, gravatarEmail } }) => (
 );
 
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(TriviaHeader);
 
-Header.propTypes = {
+TriviaHeader.propTypes = {
   score: PropTypes.number,
   name: PropTypes.string,
   gravatarEmail: PropTypes.string,
 };
 
-Header.defaultProps = {
+TriviaHeader.defaultProps = {
   score: 0,
   name: '',
   gravatarEmail: '',
