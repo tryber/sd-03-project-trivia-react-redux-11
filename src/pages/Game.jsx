a6e6dd4fc282435c7ae297f28ec809f2146f93c4
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-09 17:43:13 -0300

    Code Climate

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -7,6 +7,27 @@ import fetchTrivia from '../actions/fetchTrivia';
 const getRandomIndex = (max) => Math.round(Math.random() * max);
 
 export class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.createAnswersButtons = this.createAnswersButtons.bind(this);
+  }
+
+  createAnswersButtons() {
+    const { results } = this.props;
+    const answers = results[0].incorrect_answers.map((answer, index) => (
+      <button data-testid={`wrong-answer-${index}`} type="button" key={answer}>
+        {answer}
+      </button>
+    ));
+    answers.splice(
+      getRandomIndex(results[0].incorrect_answers.length), 0,
+      <button data-testid="correct-answer" type="button" key={results[0].correct_answer}>
+        {results[0].correct_answer}
+      </button>,
+    );
+    return answers;
+  }
+
   render() {
     const {
       token,
@@ -22,22 +43,11 @@ export class Game extends Component {
     if (tokenIsFetching || gameIsFetching) {
       return <span>Loading</span>;
     }
-    const answers = results[0].incorrect_answers.map((answer, index) => (
-      <button data-testid={`wrong-answer-${index}`} type="button" key={answer}>
-        {answer}
-      </button>
-    ));
-    answers.splice(
-      getRandomIndex(results[0].incorrect_answers.length), 0,
-      <button data-testid="correct-answer" type="button" key={results[0].correct_answer}>
-        {results[0].correct_answer}
-      </button>,
-    );
     return (
       <div className="white-text">
         <h4 data-testid="question-category">{results[0].category}</h4>
         <p data-testid="question-text">{results[0].question}</p>
-        {answers}
+        {this.createAnswersButtons()}
       </div>
     );
   }
