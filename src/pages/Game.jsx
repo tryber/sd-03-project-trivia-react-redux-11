e0c1a3f671a9c6f300a4aadfc1b543a41e1b2dd2
Henrique Rezende <henriquerezendec@gmail.com>
2020-06-10 15:48:54 -0300

    Code Climate

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -24,6 +24,8 @@ export class Game extends Component {
     this.createCorrectAnswerIndexes = this.createCorrectAnswerIndexes.bind(this);
     this.fetchTrivia = this.fetchTrivia.bind(this);
     this.timerCountdown = this.timerCountdown.bind(this);
+    this.incorrectAnswerButton = this.incorrectAnswerButton.bind(this);
+    this.correctAnswerButton = this.correctAnswerButton.bind(this);
   }
 
   componentDidMount() {
@@ -58,10 +60,9 @@ export class Game extends Component {
     });
   }
 
-  createAnswersButtons() {
-    const { results } = this.props;
-    const { correctAnswerClass, incorrectAnswerClass, randomIndexes, disableButton } = this.state;
-    const answers = results[0].incorrect_answers.map((answer, index) => (
+  incorrectAnswerButton(answer, index) {
+    const { incorrectAnswerClass, disableButton } = this.state;
+    return (
       <li>
         <button
           data-testid={`wrong-answer-${index}`}
@@ -74,10 +75,13 @@ export class Game extends Component {
           {answer}
         </button>
       </li>
-    ));
-    answers.splice(
-      randomIndexes[0],
-      0,
+    );
+  }
+
+  correctAnswerButton() {
+    const { results } = this.props;
+    const { correctAnswerClass, disableButton } = this.state;
+    return (
       <li>
         <button
           data-testid="correct-answer"
@@ -89,8 +93,16 @@ export class Game extends Component {
         >
           {results[0].correct_answer}
         </button>
-      </li>,
+      </li>
     );
+  }
+
+  createAnswersButtons() {
+    const { results } = this.props;
+    const { randomIndexes } = this.state;
+    const answers = results[0].incorrect_answers
+      .map((answer, index) => this.incorrectAnswerButton(answer, index));
+    answers.splice(randomIndexes[0], 0, this.correctAnswerButton());
     return answers;
   }
 
@@ -98,9 +110,8 @@ export class Game extends Component {
     const { randomIndexes } = this.state;
     const { results } = this.props;
     if (results.length > 0 && randomIndexes.length === 0) {
-      const index = Object.values(results).map((result) =>
-        getRandomIndex(result.incorrect_answers.length),
-      );
+      const index = Object.values(results)
+        .map((result) => getRandomIndex(result.incorrect_answers.length));
       this.setState({ randomIndexes: index });
     }
   }
