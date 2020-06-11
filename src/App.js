fbec0c4ae138ca91d1ba042c402d3f04a64bdb85
tales <talestales@gmail.com>
2020-06-11 11:32:36 -0300

    ranking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Settings from './pages/Settings';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Game from './pages/Game';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -20,6 +21,9 @@ export default function App() {
           </Route>
           <Route path="/game">
             <Game />
+          </Route>
+          <Route path="/ranking">
+            <Ranking />
           </Route>
         </Switch>
         <Footer />
---
+++ src/pages/Ranking.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Ranking extends Component {
+  ranking = {
+    name: '',
+    score: '',
+    picture: '',
+  }
+  render() {
+    const rankedLadder = localStorage.getItem('ranking') !== null ? JSON.parse(localStorage.getItem('ranking')) : [];
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <ol>
+          {rankedLadder.map(
+            (ranking, index) => (
+              <li key={`${ranking.name}_${index}`}>
+                <div>
+                  <img src={ranking.picture} alt={`${ranking.name}`} />
+                </div>
+                <div>
+                  <span data-testid={`player-name-${index}`}>
+                    {`${ranking.name} `} pontuou
+                  </span>
+                  <span data-testid={`player-score-${index}`}>
+                    {`${ranking.score}`}
+                  </span>
+                </div>
+              </li>
+            ),
+          )}
+        </ol>
+        <section>
+          <Link
+            data-testid="btn-go-home"
+            to="/"
+          >
+            INÍCIO
+            </Link>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
