fc87a9d18ad6ba71dba722ebf4e15b13aeef922e
henrique <hbaetaleite@gmail.com>
2020-06-15 17:25:10 -0300

    Prop-Types

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
@@ -126,5 +127,11 @@ const mapDispatchToProps = (dispatch) =>
     { changeCtgry: changeCategory, changeDffclty: changeDifficulty, changeTyp: changeType },
     dispatch,
   );
+
+Settings.propTypes = {
+  changeCtgry: PropTypes.func.isRequired,
+  changeTyp: PropTypes.func.isRequired,
+  changeDffclty: PropTypes.func.isRequired,
+};
 
 export default connect(null, mapDispatchToProps)(Settings);
